a329ea162cdf140ecbd29a23920beb20d67a12d1
Alejo <alejomesa.999@gmail.com>
2025-06-11 23:56:34 -0500

    fix: update search input and error handling in by-capital-page component refactor: enhance resource handling in by-capital-page component

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,7 +1,7 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, resource, signal } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
 import { CountryListComponent } from '../../components/country-list/country-list.component';
 import { SearchInputComponent } from '../../components/search-input/search-input.component';
-import type { Country } from '../../interfaces/country.interface';
 import { CountryService } from '../../services/country.service';
 
 @Component({
@@ -11,27 +11,39 @@ import { CountryService } from '../../services/country.service';
 })
 export class ByCapitalPageComponent {
   countyService = inject(CountryService);
+  query = signal('');
 
-  isLoading = signal(false);
-  isError = signal<string | null>(null);
-  countries = signal<Country[]>([]);
+  countryResource = resource({
+    request: () => ({ query: this.query() }),
+    loader: async ({ request }) => {
+      if (!request.query) return [];
 
-  onSearch(query: string) {
-    if (this.isLoading()) return;
+      return await firstValueFrom(
+        this.countyService.searchByCapital(request.query)
+      );
+    },
+  });
 
-    this.isLoading.set(true);
+  // isLoading = signal(false);
+  // isError = signal<string | null>(null);
+  // countries = signal<Country[]>([]);
 
-    this.isError.set(null);
-    this.countyService.searchByCapital(query).subscribe({
-      next: (countries) => {
-        this.isLoading.set(false);
-        this.countries.set(countries);
-      },
-      error: (err) => {
-        this.isLoading.set(false);
-        this.countries.set([]);
-        this.isError.set(err);
-      },
-    });
-  }
+  // onSearch(query: string) {
+  //   if (this.isLoading()) return;
+
+  //   this.isLoading.set(true);
+
+  //   this.isError.set(null);
+  //   this.countyService.searchByCapital(query).subscribe({
+  //     next: (countries) => {
+  //       this.isLoading.set(false);
+  //       this.countries.set(countries);
+  //     },
+  //     error: (err) => {
+  //       this.isLoading.set(false);
+  //       this.countries.set([]);
+  //       this.isError.set(err);
+  //     },
+  //   });
+  // }
 }
